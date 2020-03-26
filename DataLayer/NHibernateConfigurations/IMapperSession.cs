ae54226a968c6ef4900dba312c99e72f107aac3a
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:19:17 +0530

    files added

---
+++ DataLayer/NHibernateConfigurations/IMapperSession.cs
@@ -0,0 +1,24 @@
+﻿
+using Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.NHibernateConfigurations
+{
+
+    public interface IMapperSession
+    {
+        void BeginTransaction();
+        System.Threading.Tasks.Task Commit();
+        System.Threading.Tasks.Task Rollback();
+        void CloseTransaction();
+        System.Threading.Tasks.Task Save(List<Class1> entity);
+        System.Threading.Tasks.Task Delete(Class1 entity);
+        IQueryable<Class1> clas { get; }
+    }
+}
+
---
+++ Level1/Controllers/NewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level1.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Add XML as Input and Output Formatter.
+
+         //   Enable logging through one of these methods –
+         //Adding through pipeline and through providers.
+
+
+            return View();
+        }
+    }
+}
